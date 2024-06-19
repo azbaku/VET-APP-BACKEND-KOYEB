6fdf5d5205abe59bfee014e0de68c3062783d244
azbaku <mirza.azbaku@gmail.com>
2024-06-19 11:21:56 +0300

    Eski Tarihe Donuldu

--- src/main/java/simsek/ali/VeterinaryManagementProject/dto/request/VaccineWithoutCustomerRequestDto.java
+++ src/main/java/simsek/ali/VeterinaryManagementProject/dto/request/VaccineWithoutCustomerRequestDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,7 +14,8 @@ import lombok.Setter;
 public class VaccineWithoutCustomerRequestDto {
     private String name;
     private String code;
-    private String protectionStartDate;
-    private String protectionFinishDate;
-    private Long animalId;
+    private LocalDate protectionStartDate;
+    private LocalDate protectionFinishDate;
+    private AnimalWithoutCustomerRequestDto animalWithoutCustomer;
+    private Long reportId;
 }
